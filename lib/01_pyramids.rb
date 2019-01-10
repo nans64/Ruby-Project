9d2f0fb3fdf13ad5bc476ab3da084b78b7e246f7
nans <nans.noel@gmail.com>
2019-01-10 20:20:18 +0100

    01

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -1,10 +1,39 @@
+ def half_pyramid
+
+	# The pyramid will have 5 stairs
+
+	i = 1
+	k = 5
+	t = 5
+
+	# Construct the pyramid from the top to the bottom
+	i.upto(k) do
+	  t.times do
+	    print ' '
+	  end
+
+	  # Print the *
+	  (2 * i - 1).times do
+	    print '*'
+	  end
+	  # Return the line for the next stair
+	  print "\n"
+
+	  t -= 1
+	  i += 1
+	end
+
+end
+
+#half_pyramid ## If you unquote this sentence you will get the haf pyramid on top of everything
+
 
  # Ask how many stairs the users wants for his Pyramid.
 
 
-def combien_etages()
+def combien_etages
 
-	puts "Combien d'étages souhaites tu ?"
+	puts "Combien d'étages souhaites tu ? (nombre impair) "
 
     $a = gets.chomp.to_i
     return $a
@@ -18,12 +47,12 @@ def even?(a)
   (a % 2) == 0 
 end
 
-unless even?(combien_etages()) == false 
+unless even?(combien_etages) == false 
 
 # Reject if the number is even
  
 
-	puts "Il faut que tu mettes un nombre impair STPPEU"
+	puts "Petit malin ;) ! Pourrais tu entrer un nombre impair stp ?"
 
 else 
 
